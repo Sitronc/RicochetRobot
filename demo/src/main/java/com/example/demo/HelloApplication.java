a51acce9195ba78cb397e000992e38a2032d0022
Hugs01 <97509049+Hugs01@users.noreply.github.com>
2022-05-20 16:56:01 +0200

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.InputStream;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Graph.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 300, 300);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
